d4232097befc6d6bc903b6178ee3c9c9bd174d40
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-05 23:50:23 +0200

    retrieve the favorites recipes's user

--- src/actions/recipes.js
+++ src/actions/recipes.js
@@ -1,6 +1,7 @@
 // === action types
 export const FETCH_RECIPES = 'FETCH_RECIPES';
 export const SAVE_RECIPES = 'SAVE_RECIPES';
+export const FETCH_FAVORITES = 'FETCH_FAVORITES';
 
 // action creators
 export const fetchRecipes = () => ({
@@ -10,4 +11,8 @@ export const fetchRecipes = () => ({
 export const saveRecipes = (recipes) => ({
   type: SAVE_RECIPES,
   recipes: recipes,
+});
+
+export const fetchFavorites = () => ({
+  type: FETCH_FAVORITES,
 });
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { LOG_IN, saveUserData } from '../actions/auth';
+import { FETCH_FAVORITES, fetchFavorites } from '../actions/recipes';
 
 const authMiddleware = (store) => (next) => (action) => {
   // console.log('on a intercepté une action dans le middleware auth: ', action);
@@ -24,12 +25,39 @@ const authMiddleware = (store) => (next) => (action) => {
             response.data.logged,
           );
           store.dispatch(newAction);
+
+          store.dispatch(fetchFavorites());
         })
         .catch((error) => {
           console.log(error);
         });
       break;
     }
+
+    case FETCH_FAVORITES:
+      // console.log('on va aller chercher les recettes préférées');
+
+      // on envoie le token : dans le header Authorization, en précisant le mot
+      // "Bearer" avant le token
+      axios.post(
+        // URL
+        'http://localhost:3001/favorites',
+        // données
+        {},
+        // options, notamment les headers
+        {
+          headers: {
+            Authorization: `Bearer ${store.getState().auth.token}`,
+          },
+        },
+      )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      break;
 
     default:
   }
