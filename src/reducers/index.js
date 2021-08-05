23ebbb1663607fc31086658a4c54b9aed43659cd
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-05 23:26:17 +0200

    start inputs control

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import recipesReducer from './recipesReducer';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   recipes: recipesReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
